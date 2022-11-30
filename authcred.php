22db30c5b78f3866eb87593b6feda889d9ab1b48
iniznet <iniznet@gmail.com>
2022-11-30 17:18:54 +0000

    bump version

--- authcred.php
+++ authcred.php
@@ -3,7 +3,7 @@
  * Plugin Name:       AuthCRED
  * Plugin URI:        https://github.com/iniznet/authcred
  * Description:       Provide simple authentication alongside mycred integration with shortcodes
- * Version:           1.1.0
+ * Version:           1.2.0
  * Requires at least: 5.8
  * Requires PHP:      7.2
  * Author:            niznet
