ee481ec180b2e5b2340d0bc091e41b0acbf52c3a
iniznet <iniznet@gmail.com>
2022-12-04 17:33:01 +0000

    Cleaning up unnecessary files for WordPress repository

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,8 +2,10 @@
 
 shopt -s extglob nullglob
 
-rm -rf scripts .github node_modules .git .gitignore
+rm -rf scripts .github .editorconfig .git .gitignore
+rm -rf vite.config.js tailwind.config.js renovate.json readme.md postcss.config.js
+rm -rf plugin.json package-lock.json package.json composer.lock composer.json CHANGELOG.md
+rm -rf node_modules resources
 
 mkdir authcred
 mv !(authcred) authcred
-mv .editorconfig authcred
